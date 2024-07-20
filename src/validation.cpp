1556693cdc27402bb653f543ae59a7f351f5719a
Matheus Duarte <matheusduarte@psr-inc.com>
2024-07-20 17:08:53 -0300

    Change initial subsidy

--- src/validation.cpp
+++ src/validation.cpp
@@ -1642,8 +1642,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    CAmount nSubsidy = 100 * COIN;
+    // Subsidy is cut in half every 50050 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
 }
